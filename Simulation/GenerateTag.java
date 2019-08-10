e1e050f064b6698fe9e48955751d5a92cb7c9a8f
Uzer Jamal <uzerjamal@gmail.com>
2019-08-10 20:40:17 +0530

    Slightly modified GenerateTag.java

--- Simulation/GenerateTag.java
+++ Simulation/GenerateTag.java
@@ -3,16 +3,31 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 class Generate{
-    String generateId(){
+    private String[] generateTagId(int numOfId){
+        String[] id = new String[numOfId];
         Random rn = new Random();
         String batchNumber = "E2005176806060261";
-        int firstHalf = (int) (rn.nextInt(9999 - 1000) + 1000);
+        int firstHalf;
         String secondHalf = "";
-        for(int i=0; i<3; i++){
-            secondHalf+= (char)((int) rn.nextInt(90 - 65) + 65);
+        for(int i=0; i<numOfId; i++){
+            firstHalf = (int) (rn.nextInt(9999 - 1000) + 1000);
+            for(int j=0; j<3; j++){
+                secondHalf+= (char)((int) rn.nextInt(90 - 65) + 65);
+            }
+            id[i] = batchNumber + firstHalf + secondHalf;
+            secondHalf = "";
         }
-        String id = batchNumber + firstHalf + secondHalf;
         return id;
+    }
+
+    String[] generateId(int numOfId){
+        String[] id = new String[numOfId];
+        id = generateTagId(numOfId);
+        return id;
+    }
+
+    String generateId(){
+        return generateId(1)[0];
     }
 
     int readerNumber(){
@@ -32,11 +47,17 @@ class Generate{
 class GenerateTag{
     public static void main(String[] args){
         Generate tag = new Generate();
-        for(int i=0; i<20; i++){
-            String generatedTag = tag.generateId();
-            int readerNum = tag.readerNumber();
-            String time = tag.time();
-            System.out.println(generatedTag + "\t" + readerNum + "\t" + time);
-        }
+        String[] id = tag.generateId(5);
+        System.out.println(tag.generateId() + " " + tag.readerNumber() + " " + tag.time());
+        System.out.println("=====");
+        for(int i=0; i<5; i++)
+            System.out.println(id[i] + " " + tag.readerNumber() + " " + tag.time());
+        
+        //for(int i=0; i<20; i++){
+            //String generatedTag = tag.generateId();
+            //int readerNum = tag.readerNumber();
+            //String time = tag.time();
+            //System.out.println(generatedTag + "\t" + readerNum + "\t" + time);
+        //}
     }
 }
