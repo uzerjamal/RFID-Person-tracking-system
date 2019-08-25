2a8bd4ddc6d78053a84083579e15f89fefecf845
chikky_ck <33838425+chikky-ck@users.noreply.github.com>
2019-08-25 22:15:50 +0530

    Add files via upload

---
+++ Website/parent/sidebar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<!-- Sidebar -->
+<div class="w3-sidebar w3-bar-block w3-border-right" style="display:none" id="mySidebar">
+  <button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button><br>
+  <a href="#" class="w3-bar-item w3-button">Profile</a>
+  <a href="#" class="w3-bar-item w3-button">Child profile</a>
+  <a href="#" class="w3-bar-item w3-button">My Tag</a>
+</div>
+
+<!-- Page Content -->
+<div class="w3-teal">
+  <button class="w3-button w3-teal w3-xlarge" onclick="w3_open()">☰ Parents Contents</button>
+</div>
+
+<script>
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
+     
+</body>
+</html> 
