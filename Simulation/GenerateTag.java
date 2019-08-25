673c6ce632c5e185b9006d4ca4ec1425eafba689
kaajaljha <53533328+kaajaljha@users.noreply.github.com>
2019-08-25 19:45:10 +0530

    Update GenerateTag.java

--- Simulation/GenerateTag.java
+++ Simulation/GenerateTag.java
@@ -1,42 +1,73 @@
 import java.util.Random;
-import java.util.Date;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
-class Generate{
-    String generateId(){
+class Tag
+{
+   private String[] generateTags(int numOfId)
+   {
+        String[] id = new String[numOfId];
         Random rn = new Random();
         String batchNumber = "E2005176806060261";
-        int firstHalf = (int) (rn.nextInt(9999 - 1000) + 1000);
+        int firstHalf;
         String secondHalf = "";
-        for(int i=0; i<3; i++){
-            secondHalf+= (char)((int) rn.nextInt(90 - 65) + 65);
-        }
-        String id = batchNumber + firstHalf + secondHalf;
-        return id;
-    }
-
-    int readerNumber(){
-        Random rn = new Random();
-        return (int)(rn.nextInt(5));
-    }
-
-    String time(){
-        Date objDate = new Date();
-        String strDateFormat = "hh:mm:ss a dd-MM-yyyy";
-        SimpleDateFormat objSDF = new SimpleDateFormat(strDateFormat);
-
-        return objSDF.format(objDate);
-    }
+        for(int i=0; i<numOfId; i++){
+            firstHalf = (int) ( rn.nextInt(9999 - 1000) + 1000);
+            for(int j=0; j<3; j++){
+                secondHalf+= (char)((int) rn.nextInt(90 - 65) + 65);            }
+            id[i] = batchNumber + firstHalf + secondHalf;
+            secondHalf = "";
+          }
+        return id; 
 }
 
+ String[] generateId(int numOfId)
+        {
+            String[] id = new String[numOfId];
+            id = generateTags(numOfId);
+            return id; 
+       }
+String generateId()
+        {
+            return generateTags(1)[0];    
+        }
+
+int generateReaderNumber()
+      {
+        Random rn = new Random();
+        return (int)(rn.nextInt(5));  
+      }
+String time()
+      {
+         String i="null";
+      String j ="null";
+      String k ="null";
+      Random r2 = new Random();
+      int h = r2.nextInt((13-8)+1)+8;
+      int min = r2.nextInt((60-0)+1)+0;
+      int sec = r2.nextInt((60-0)+1)+0;
+      j = String.valueOf(sec);
+      k =  String.valueOf(min);
+      i = String.valueOf(h);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+  LocalDateTime localDate = LocalDateTime.now();
+         
+      return (i+":"+j+":"+k+"  "+dtf.format(localDate));
+
+      }
+    }
 class GenerateTag{
     public static void main(String[] args){
-        Generate tag = new Generate();
-        for(int i=0; i<20; i++){
-            String generatedTag = tag.generateId();
-            int readerNum = tag.readerNumber();
-            String time = tag.time();
-            System.out.println(generatedTag + "\t" + readerNum + "\t" + time);
-        }
-    }
-}
+        Tag tag = new Tag();
+        String[] id = tag.generateId(5);
+        System.out.println(tag.generateId() + " " + tag.generateReaderNumber() + " " + tag.time());
+        System.out.println("=====");
+        for(int i=0; i<5; i++)
+            System.out.println(id[i] + " " + tag.generateReaderNumber() + " " + tag.time());
+        /*Database db = new Database(); / 
+        db.connect();//
+                db.addUnallocatedTags(id);*/    }}
