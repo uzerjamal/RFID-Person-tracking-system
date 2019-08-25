297fe4129743e8faeba1b89d39d793a0fb495b2e
kaajaljha <53533328+kaajaljha@users.noreply.github.com>
2019-08-25 21:04:00 +0530

    Update GenerateTag.java

--- Simulation/GenerateTag.java
+++ Simulation/GenerateTag.java
@@ -75,9 +75,9 @@ int generateReaderNumber()
       }
 String time()
       {
-         String hour="null";
-      String minute ="null";
-      String second ="null";
+         String hour="";
+      String minute ="";
+      String second ="";
       Random r2 = new Random();
       int hr = r2.nextInt((13-8)+1)+8;
       int min = r2.nextInt((60-0)+1)+0;
