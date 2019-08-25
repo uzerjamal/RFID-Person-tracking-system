5054ad776ddc9ea882c49aa18d6795052c63dca4
kaajaljha <53533328+kaajaljha@users.noreply.github.com>
2019-08-25 19:39:50 +0530

    Add files via upload

--- GenerateTag.java
+++ GenerateTag.java
@@ -1,10 +1,7 @@
 import java.util.Random;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 import java.util.Date;
+import java.text.SimpleDateFormat;
+
 class Generate{
     String generateId(){
         Random rn = new Random();
@@ -17,30 +14,20 @@ class Generate{
         String id = batchNumber + firstHalf + secondHalf;
         return id;
     }
+
     int readerNumber(){
         Random rn = new Random();
         return (int)(rn.nextInt(5));
     }
+
     String time(){
-      String i="null";
-      String j ="null";
-      String k ="null";
-      Random r2 = new Random();
-      int h = r2.nextInt((13-8)+1)+8;
-      int min = r2.nextInt((60-0)+1)+0;
-      int sec = r2.nextInt((60-0)+1)+0;
-      j = String.valueOf(sec);
-      k =  String.valueOf(min);
-      i = String.valueOf(h);
-          
-   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-  LocalDateTime localDate = LocalDateTime.now();
-         
-      return (i+":"+j+":"+k+"  "+dtf.format(localDate));
+        Date objDate = new Date();
+        String strDateFormat = "hh:mm:ss a dd-MM-yyyy";
+        SimpleDateFormat objSDF = new SimpleDateFormat(strDateFormat);
+
+        return objSDF.format(objDate);
+    }
 }
-     
-}
- 
 
 class GenerateTag{
     public static void main(String[] args){
