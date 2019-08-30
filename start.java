da19c316d2a50e535f571d3bd1132e7fb137c5d1
Uzer Jamal <uzerjamal@gmail.com>
2019-08-30 23:42:02 +0530

    Refactored code

---
+++ start.java
@@ -0,0 +1,16 @@
+import database.Database;
+import simulation.Tag;
+
+class start{
+    public static void main(String[] args){
+        Tag tag = new Tag();
+        String[] id = tag.generateId(2);
+        System.out.println(tag.generateId() + " " + tag.generateReaderNumber() + " " + tag.time());
+        System.out.println("=====");
+        for(int i=0; i<2; i++)
+        System.out.println(id[i] + " " + tag.generateReaderNumber() + " " + tag.time());
+        Database db = new Database();
+        db.connect();
+        db.addUnallocatedTags(id);
+    }
+}
