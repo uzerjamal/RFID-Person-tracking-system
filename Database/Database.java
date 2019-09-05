3ff2b7021641e6ae316ddfb148aca5ae96048740
Uzer Jamal <uzerjamal@gmail.com>
2019-09-06 00:15:51 +0530

    Assign tag ID to Students. Closes #17

--- Database/Database.java
+++ Database/Database.java
@@ -9,21 +9,21 @@ public class Database{
     private String pass = "zxcvb321";   //TODO ask password at run time(in constructor) to increase security
     private String dbDriver = "jdbc:mysql://db4free.net:3306/rfidproj";
     private Connection conn = null;
+    private Statement stmt = null;
 
     public void connect(){
         try {
             conn = DriverManager.getConnection(dbDriver, user, pass);
             System.out.println("connected");
+            stmt = conn.createStatement();
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
         }
     }
 
     public void addUnallocatedTags(String[] tag){
-        Statement stmt = null;
         for(int i = 0; i<tag.length; i++){
             try {
-                stmt = conn.createStatement();
                 stmt.executeUpdate("INSERT INTO Unallocated_Tags (TagID) VALUES ('" + tag[i] + "')");
             }
             catch (SQLException ex){
@@ -40,6 +40,26 @@ public class Database{
         Random rn = new Random();
         return Calendar.getInstance().get(Calendar.YEAR) + String.valueOf(rn.nextInt(99999));
     }
+
+    public void assignTag(String studentClass, String studentName){
+        String sql = "SELECT TagID FROM Unallocated_Tags LIMIT 1";
+        String tag = "TEST";
+        ResultSet tagQuery = null;
+        try{
+            tagQuery = stmt.executeQuery(sql);
+            tagQuery.next();
+            tag = tagQuery.getString("TagID");
+        }
+        catch(Exception e){System.out.println(e);}
+        sql = "UPDATE Student_Details SET Tag_Id='" + tag +"' WHERE Student_name='"+ studentName +"' AND Class='"+ studentClass + "'";
+        try{
+            stmt.executeUpdate(sql);
+        }
+        catch(SQLException ex){
+            System.out.println(ex);
+        }
+        System.out.println(tag);
+    }
     
     public void addStudentRecord(String studentClass, int rollNo, String studentName, String parentName){
         Statement stmt = null;
@@ -47,7 +67,7 @@ public class Database{
         "(Class, Roll_no, Student_name, Parent_name, Parent_Id) " +
         "VALUES ('%s', %s, '%s', '%s', '%s');", studentClass, rollNo, studentName, parentName, generateParentId());
         try {
-            stmt = conn.createStatement();
+
             stmt.executeUpdate(insertQuery);
         }
         catch (SQLException ex){
